a98c692eba700dacc763776e1eb66d77d6e8a38e
y0329 <55629324+y0329@users.noreply.github.com>
2022-06-16 15:30:56 +0900

    不要なファイルを削除

--- bus_backend.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-system = FastAPI()
-ride_bus_list = []
-@system.get("user_id/{user_id}/time/{time}")
-def bus_line_count(user_id,time):
-        ride_bus_list.append({'user_id': user_id,'ride_time':time})
-        return len(ride_bus_list)
