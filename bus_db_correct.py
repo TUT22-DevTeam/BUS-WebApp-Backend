c63ded9f78d62bb1f6082adf910c0164182dfbfb
y0329 <55629324+y0329@users.noreply.github.com>
2022-06-18 19:52:56 +0900

    完成版

--- bus_db_correct.py
+++ bus_db_correct.py
@@ -2,27 +2,71 @@ import pymysql.cursors
 import json
 from datetime import datetime, date, time
 from datetime import timedelta
-interval_minute = 10#単位時間
-today_list = []
-count = 0
-json_list_h=[]
-json_list_m=[]
+hachioji_list=[]
+minamino_list=[]
+now_list = []
+json_list_h=[
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0] 
+   ]
+json_list_m=[
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0], 
+     [0,0,0,0,0,0] 
+   ]
 connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password='linuxclub!',
                                  database='bus_db',
                                  cursorclass=pymysql.cursors.DictCursor)
 cursor = connection.cursor()
-sql = "SELECT * FROM  passenger"
+sql = "SELECT * FROM  passenger ORDER BY date"
 cursor.execute(sql)
 result = cursor.fetchall()
-today_date = datetime.combine(date.today(), time())
-tomorrow_date = today_date+timedelta(days=1)
+print(result)
 now = datetime.now()
-now.replace(minute=now.minute - now.minute % interval_minute, second=0, microsecond=0)
+print(now)
+now = now.replace(minute=0,second=0,microsecond=0)
+print(now)
+now-=timedelta(hours=16)
+print(now)
 for i in range(len(result)):
-    if today_date < result[i]["date"] < tomorrow_date:
-        today_list.append(result[i])
-for i in range(len(today_list)):
-    if today_list[i]["station"] =="minamino":
-        json_list_m.append(today_list[i])
+    if now < result[i]["date"] < now+timedelta(hours=5):
+        now_list.append(result[i])
+for i in range(len(now_list)):
+    if now_list[i]["station"] =="hachioji":
+        hachioji_list.append(now_list[i])
+    else:
+        minamino_list.append(now_list[i])
+print(minamino_list)
+for i in range(len(minamino_list)):
+    comparison_variable = now
+    for j in range(36):
+        print(comparison_variable,comparison_variable+timedelta(minutes=10))
+        if comparison_variable<= minamino_list[i]["date"] <= comparison_variable+timedelta(minutes=10):
+            print(minamino_list[i],"true")
+            json_list_m[int(j/6)][j%6]+=1 
+        comparison_variable+=timedelta(minutes=10)
+for i in range(len(hachioji_list)):
+    comparison_variable = now
+    for j in range(36):
+        print(comparison_variable,comparison_variable+timedelta(minutes=10))
+        if comparison_variable<= minamino_list[i]["date"] <= comparison_variable+timedelta(minutes=10):
+            print(hachioji_list[i],"true")
+            json_list_h[int(j/6)][j%6]+=1 
+        comparison_variable+=timedelta(minutes=10)
+print(json_list_m,json_list_h)
+with open('minamino.json', 'w') as f:
+    json.dump(json_list_m, f, ensure_ascii=False, indent=4)
+with open('hachioji.json', 'w') as f:
+    json.dump(json_list_h, f, ensure_ascii=False, indent=4)  
+# 接続を切断
+cursor.close()
+connection.close()
