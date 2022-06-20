26d993160d714ebaca3586174286d9b99b7eec74
y0329 <55629324+y0329@users.noreply.github.com>
2022-06-20 13:46:01 +0900

    ヘッダー情報を追加

--- bus_db_insert.py
+++ bus_db_insert.py
@@ -2,6 +2,13 @@ import pymysql.cursors
 from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 class json_data(BaseModel):
     date: str
     hours: str
