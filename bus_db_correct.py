a1b33d3d9041087227faf34eeab2dccf3f83e60a
y0329 <55629324+y0329@users.noreply.github.com>
2022-06-20 09:48:01 +0900

    検証用のprintを削除、かつカウントのバグを修正
    30分などの境界に当たる時刻を使用するとカウント数が増加してしまうバグがあったので、上限のほうを以下ではなく未満に修正することで対応した

--- bus_db_correct.py
+++ bus_db_correct.py
@@ -30,11 +30,8 @@ cursor = connection.cursor()
 sql = "SELECT * FROM  passenger ORDER BY date"
 cursor.execute(sql)
 result = cursor.fetchall()
-print(result)
 now = datetime.now()
-print(now)
 now = now.replace(minute=0,second=0,microsecond=0)
-print(now)
 for i in range(len(result)):
     if now < result[i]["date"] < now+timedelta(hours=5):
         now_list.append(result[i])
@@ -43,24 +40,18 @@ for i in range(len(now_list)):
         hachioji_list.append(now_list[i])
     else:
         minamino_list.append(now_list[i])
-print(minamino_list)
 for i in range(len(minamino_list)):
     comparison_variable = now
     for j in range(36):
-        print(comparison_variable,comparison_variable+timedelta(minutes=10))
-        if comparison_variable<= minamino_list[i]["date"] <= comparison_variable+timedelta(minutes=10):
-            print(minamino_list[i],"true")
+        if comparison_variable<= minamino_list[i]["date"] < comparison_variable+timedelta(minutes=10):
             json_list_m[int(j/6)][j%6]+=1 
         comparison_variable+=timedelta(minutes=10)
 for i in range(len(hachioji_list)):
     comparison_variable = now
     for j in range(36):
-        print(comparison_variable,comparison_variable+timedelta(minutes=10))
-        if comparison_variable<= minamino_list[i]["date"] <= comparison_variable+timedelta(minutes=10):
-            print(hachioji_list[i],"true")
+        if comparison_variable<= minamino_list[i]["date"] < comparison_variable+timedelta(minutes=10):
             json_list_h[int(j/6)][j%6]+=1 
         comparison_variable+=timedelta(minutes=10)
-print(json_list_m,json_list_h)
 with open('minamino.json', 'w') as f:
     json.dump(json_list_m, f, ensure_ascii=False, indent=4)
 with open('hachioji.json', 'w') as f:
