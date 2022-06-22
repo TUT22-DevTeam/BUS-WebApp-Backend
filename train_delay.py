101714d19f29cfc48ead5ff0108711b54d611767
y0329 <55629324+y0329@users.noreply.github.com>
2022-06-22 15:17:48 +0900

    Update train_delay.py

--- train_delay.py
+++ train_delay.py
@@ -1,4 +1,23 @@
-from selenium import webdriver By
-driver = webdriver.Chrome()
-driver.get("https://transit.yahoo.co.jp/diainfo/31/0")
-print(driver.findElement(By.className("normal")))
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import json
+info_list=[[0,0],[0,0]]
+url_list=["31","38"]
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+for i in range(len(url_list)):
+    driver.get("https://transit.yahoo.co.jp/diainfo/"+url_list[i]+"/0")
+    if len(driver.find_elements_by_css_selector(".trouble")) > 0:
+        info_list[i][0]=1
+        info_list[i][1]=driver.find_elements_by_css_selector(".trouble")[0].text
+    elif len(driver.find_elements_by_css_selector(".normal")) > 0:
+        info_list[i][0]=0
+        info_list[i][1]="正常に運行中"
+    else:
+        info_list[i][0]=2
+        info_list[i][0]="取得に失敗"
+with open("train_delay.json","w") as j:
+    json.dump(info_list,j,indent=4,ensure_ascii=False)
+driver.quit()
+print(info_list)
