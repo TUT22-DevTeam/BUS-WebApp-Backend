5b7c250dec4666a3c8028e914b8560f691f6114a
y0329 <55629324+y0329@users.noreply.github.com>
2022-07-08 16:57:43 +0900

    CSSセレクターがそのままで使用できなかったのでByオプションを追加して対応

--- train_delay.py
+++ train_delay.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
+from selenium.webdriver.common.by import By
 info_list=[[0,0],[0,0]]
 url_list=["31","38"]
 options = Options()
@@ -8,10 +9,10 @@ options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 for i in range(len(url_list)):
     driver.get("https://transit.yahoo.co.jp/diainfo/"+url_list[i]+"/0")
-    if len(driver.find_elements_by_css_selector(".trouble")) > 0:
+    if len(driver.find_elements(By.CSS_SELECTOR,".trouble")) > 0:
         info_list[i][0]=1
         info_list[i][1]=driver.find_elements_by_css_selector(".trouble")[0].text
-    elif len(driver.find_elements_by_css_selector(".normal")) > 0:
+    elif len(driver.find_elements(By.CSS_SELECTOR,".normal")) > 1:
         info_list[i][0]=0
         info_list[i][1]="正常に運行中"
     else:
